1e8abcbe10b144d886add1429a49f3feec68c590
tahakhan1010 <tahakhan1010@yahoo.com>
2019-02-27 10:45:49 -0500

    changing front output

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            i donno. lets check!.
+            hope this works!.
           </p>
           <a
             className="App-link"
